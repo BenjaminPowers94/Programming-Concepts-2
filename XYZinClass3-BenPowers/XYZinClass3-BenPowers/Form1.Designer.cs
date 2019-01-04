d52e17793e654ba1420830bef34d9025397887f1
Benjamin Powers <41301379+BenjaminPowers94@users.noreply.github.com>
2019-01-04 15:39:29 -0500

    Add files via upload

---
+++ XYZinClass3-BenPowers/XYZinClass3-BenPowers/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace XYZinClass3_BenPowers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.insideXTextBox = new System.Windows.Forms.TextBox();
+            this.insideYTextBox = new System.Windows.Forms.TextBox();
+            this.insideZTextBox = new System.Windows.Forms.TextBox();
+            this.asdasds = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.displayRich = new System.Windows.Forms.RichTextBox();
+            this.outsideTextBoxX = new System.Windows.Forms.TextBox();
+            this.outsideTextBoxY = new System.Windows.Forms.TextBox();
+            this.outsideTextBoxZ = new System.Windows.Forms.TextBox();
+            this.asfddsafdsf = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // insideXTextBox
+            // 
+            this.insideXTextBox.Location = new System.Drawing.Point(70, 101);
+            this.insideXTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.insideXTextBox.Name = "insideXTextBox";
+            this.insideXTextBox.Size = new System.Drawing.Size(48, 20);
+            this.insideXTextBox.TabIndex = 0;
+            // 
+            // insideYTextBox
+            // 
+            this.insideYTextBox.Location = new System.Drawing.Point(70, 143);
+            this.insideYTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.insideYTextBox.Name = "insideYTextBox";
+            this.insideYTextBox.Size = new System.Drawing.Size(48, 20);
+            this.insideYTextBox.TabIndex = 1;
+            // 
+            // insideZTextBox
+            // 
+            this.insideZTextBox.Location = new System.Drawing.Point(70, 188);
+            this.insideZTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.insideZTextBox.Name = "insideZTextBox";
+            this.insideZTextBox.Size = new System.Drawing.Size(48, 20);
+            this.insideZTextBox.TabIndex = 2;
+            // 
+            // asdasds
+            // 
+            this.asdasds.AutoSize = true;
+            this.asdasds.Location = new System.Drawing.Point(86, 84);
+            this.asdasds.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.asdasds.Name = "asdasds";
+            this.asdasds.Size = new System.Drawing.Size(14, 13);
+            this.asdasds.TabIndex = 3;
+            this.asdasds.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 127);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Z";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcButton.Location = new System.Drawing.Point(307, 127);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(130, 58);
+            this.calcButton.TabIndex = 8;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // displayRich
+            // 
+            this.displayRich.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayRich.Location = new System.Drawing.Point(70, 238);
+            this.displayRich.Name = "displayRich";
+            this.displayRich.Size = new System.Drawing.Size(255, 369);
+            this.displayRich.TabIndex = 0;
+            this.displayRich.Text = "";
+            // 
+            // outsideTextBoxX
+            // 
+            this.outsideTextBoxX.Location = new System.Drawing.Point(171, 101);
+            this.outsideTextBoxX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.outsideTextBoxX.Name = "outsideTextBoxX";
+            this.outsideTextBoxX.Size = new System.Drawing.Size(99, 20);
+            this.outsideTextBoxX.TabIndex = 9;
+            // 
+            // outsideTextBoxY
+            // 
+            this.outsideTextBoxY.Location = new System.Drawing.Point(171, 150);
+            this.outsideTextBoxY.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.outsideTextBoxY.Name = "outsideTextBoxY";
+            this.outsideTextBoxY.Size = new System.Drawing.Size(99, 20);
+            this.outsideTextBoxY.TabIndex = 10;
+            // 
+            // outsideTextBoxZ
+            // 
+            this.outsideTextBoxZ.Location = new System.Drawing.Point(171, 192);
+            this.outsideTextBoxZ.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.outsideTextBoxZ.Name = "outsideTextBoxZ";
+            this.outsideTextBoxZ.Size = new System.Drawing.Size(99, 20);
+            this.outsideTextBoxZ.TabIndex = 11;
+            // 
+            // asfddsafdsf
+            // 
+            this.asfddsafdsf.AutoSize = true;
+            this.asfddsafdsf.Location = new System.Drawing.Point(171, 84);
+            this.asfddsafdsf.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.asfddsafdsf.Name = "asfddsafdsf";
+            this.asfddsafdsf.Size = new System.Drawing.Size(72, 13);
+            this.asfddsafdsf.TabIndex = 12;
+            this.asfddsafdsf.Text = "X Axis Length";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(171, 133);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Y Axis Height";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 174);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Z Axis Depth";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(363, 271);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(430, 238);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 641);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.asfddsafdsf);
+            this.Controls.Add(this.outsideTextBoxZ);
+            this.Controls.Add(this.outsideTextBoxY);
+            this.Controls.Add(this.outsideTextBoxX);
+            this.Controls.Add(this.displayRich);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.asdasds);
+            this.Controls.Add(this.insideZTextBox);
+            this.Controls.Add(this.insideYTextBox);
+            this.Controls.Add(this.insideXTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox insideXTextBox;
+        private System.Windows.Forms.TextBox insideYTextBox;
+        private System.Windows.Forms.TextBox insideZTextBox;
+        private System.Windows.Forms.Label asdasds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.RichTextBox displayRich;
+        private System.Windows.Forms.TextBox outsideTextBoxX;
+        private System.Windows.Forms.TextBox outsideTextBoxY;
+        private System.Windows.Forms.TextBox outsideTextBoxZ;
+        private System.Windows.Forms.Label asfddsafdsf;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ XYZinClass3-BenPowers/XYZinClass3-BenPowers/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XYZinClass3_BenPowers
+{
+    public partial class Form1 : Form
+    {
+        int insX = 0; // inside x
+        int insY = 0; // inside y
+        int insZ = 0; // inside z
+        int outsideX = 0; // outside x
+        int outsideY = 0; // outisde y
+        int outsideZ = 0; // outside z
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Point // first class "Point"
+        {
+            private int x; // get set x
+            private int y; // get set y
+            private int z; // get set z
+
+            public Point (int pX, int pY, int pZ) // constructor
+            {
+                x = pX;
+                y = pY;
+                z = pZ;
+            }
+
+            public int X // get set for x
+            {
+                get
+                {
+                    return x;
+                }
+                set
+                {
+                    x = value;
+                }
+            }
+
+            public int Y // get set for y
+            {
+                get
+                {
+                    return y;
+                }
+                set
+                {
+                    y = value;
+                }
+            }
+
+            public int Z // get set for z
+            {
+                get
+                {
+                    return z;
+                }
+                set
+                {
+                    z = value;
+                }
+            }
+        }
+
+        public class Line // second class "Line"
+        {
+            private Point pt1; // get set for point 1
+            private Point pt2; // get set for point 2
+            private Point pt3; // get set for point 3
+            private Point pt4; // get set for point 4
+
+            public Point Pt1 // get set for point 1
+            {
+                get { return pt1; }
+                set { pt1 = value; }
+            }
+            public Point Pt2 // get set for point 2
+            {
+                get { return pt2; }
+                set { pt2 = value; }
+            }
+            public Point Pt3 // get set for point 3
+            {
+                get { return pt3; }
+                set { pt3 = value; }
+            }
+            public Point Pt4 // get set for point 4
+            {
+                get { return pt4; }
+                set { pt4 = value; }
+            }
+
+            public int volumeCalc(Point pt1, Point pt2, Point pt3, Point pt4) // calculates volume for shape
+            {
+                return ((pt3.X - pt1.X) * (pt2.Y - pt1.Y) * (pt4.Z - pt1.Z));
+            }
+        }
+
+        private void calcButton_Click(object sender, EventArgs e) // calculate button 
+        {
+            insX = int.Parse(insideXTextBox.Text); // calling int insX(insideX) and parsing it to insideTextBoxX
+            insY = int.Parse(insideYTextBox.Text); // calling int insY(insideY) and parsing it to insideTextBoxY
+            insZ = int.Parse(insideZTextBox.Text); // calling int insZ(insideZ) and parsing it to insideTextBoxZ
+            outsideX = int.Parse(outsideTextBoxX.Text); // calling int outsideX and parsing it to outsideTextBoxX
+            outsideY = int.Parse(outsideTextBoxY.Text); // calling int outsideY and parsing it to outsideTextBoxY
+            outsideZ = int.Parse(outsideTextBoxZ.Text); // calling int outsideZ and parsing it to outsideTextBoxZ
+
+            Point pointA = new Point(insX, insY, insZ); // ask jaey or bill what this is doing 
+            Point pointB = new Point(insX, (insY + outsideY), insZ); // ask jaey or bill what this is doing 
+            Point pointC = new Point((insX + outsideX), (insY + outsideY), insZ); // ask jaey or bill what this is doing 
+            Point pointD = new Point((insX + outsideX), insY, insZ); // ask jaey or bill what this is doing 
+
+            Point pointE = new Point(insX, insY, (insZ + outsideZ)); // ask jaey or bill what this is doing 
+            Point pointF = new Point(insX, (insY + outsideY), (insZ + outsideZ)); // ask jaey or bill what this is doing 
+            Point pointG = new Point((insX + outsideX), (insY + outsideY), (insZ + outsideZ)); // ask jaey or bill what this is doing 
+            Point pointH = new Point((insX + outsideX), insY, (insZ + outsideZ)); // ask jaey or bill what this is doing 
+
+            Line myLine = new Line(); // calling Line Class to buttonCal
+
+            int saABCD = 0; // surface area int for points A B C D
+            int saBCFG = 0; // surface area int for points B C F G
+            int saCDGH = 0; // surface area int for points C D G H
+            int vol = 0; // volume used to calculate the volume of the shape
+
+            displayRich.Text = "X Y Z ORDER: \n\n" +
+                            "A: " + pointA.X + ", " + pointA.Y + ", " + pointA.Z + "\n" + // concatatnating 3 points 
+                            "B: " + pointB.X + ", " + pointB.Y + ", " + pointB.Z + "\n" + // concatatnating 3 points 
+                            "C: " + pointC.X + ", " + pointC.Y + ", " + pointC.Z + "\n" + // concatatnating 3 points 
+                            "D: " + pointD.X + ", " + pointD.Y + ", " + pointD.Z + "\n" + // concatatnating 3 points 
+                            "E: " + pointE.X + ", " + pointE.Y + ", " + pointE.Z + "\n" + // concatatnating 3 points 
+                            "F: " + pointF.X + ", " + pointF.Y + ", " + pointF.Z + "\n" + // concatatnating 3 points 
+                            "G: " + pointG.X + ", " + pointG.Y + ", " + pointG.Z + "\n" + // concatatnating 3 points 
+                            "H: " + pointH.X + ", " + pointH.Y + ", " + pointH.Z + "\n"; // concatatnating 3 points 
+
+            saABCD = calculateSurfaceArea((pointB.Y - pointA.Y), (pointD.X - pointA.X)); // using intergers and combining two classes together to calc surface area
+            saBCFG = calculateSurfaceArea((pointF.Z - pointB.Z), (pointG.X - pointF.X)); // using intergers and combining two classes together to calc surface area
+            saCDGH = calculateSurfaceArea((pointC.Y - pointD.Y), (pointH.Z - pointD.Z)); // using intergers and combining two classes together to calc surface area
+
+            // displaying surface area 
+            displayRich.Text += "\nSurface Area: \n\n" +
+                                        "Surface ABCD: " + saABCD + "\n" + 
+                                        "Surface BCFG: " + saBCFG + "\n" +
+                                        "Surface CDGH: " + saCDGH + "\n";
+
+            vol = myLine.volumeCalc(pointA, pointB, pointD, pointE); // using volume calc to calculate the surface area
+
+            displayRich.Text += "\nVolume: \n\n" +
+                                        "Volume: " + vol + "\n";
+        }
+
+        public int calculateSurfaceArea(int a, int b) // surface area calculation
+        {
+            int sa = 0;
+
+            sa = a * b;
+
+            return sa;
+        }
+    }
+}
